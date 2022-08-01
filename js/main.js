5faf2396ef1f24ceb8e4b053c18264e5b3f32593
yumsn <gin728emma218@gmail.com>
2022-08-01 23:00:36 +0900

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -13,6 +13,31 @@
     mask.classList.toggle('active');
   });
 
+  //スムーススクロール
+  const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+
+  for(let i = 0; i < smoothScrollTrigger.length; i++) {
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+      // console.log(targetElement);
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      // const gap = 60;
+      const target = rect + offset; //- gap;
+      header.classList.remove('active');
+      hamburger.classList.remove('active');
+      nav.classList.remove('active');
+      mask.classList.remove('active');
+
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
+    });
+  }
+
   //スライド
   $('.pickup__slider').slick({
     arrows: false,
